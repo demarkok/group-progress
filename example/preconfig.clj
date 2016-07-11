0aa219992f67e0c7d1e431528688eb9efa113f4c
Oleg Pestov <oleg.pestov@gmail.com>
2016-07-11 11:49:38 +0400

    Update preconfig.clj

--- example/preconfig.clj
+++ example/preconfig.clj
@@ -1,25 +1,30 @@
-[{:id "prostokvashino"
-  :name "Простоквашино"
+[
+{
+  :id "prostokvashino",
+  :name "Простоквашино", 
   :users
-  [{:name "Дядя Федор" :mccme 1}
-   {:name "Матроскин" :mccme 2}
-   {:name "Шарик" :mccme 3}
-   {:name "Печкин" :mccme 4}]
+  [
+    {:name "Дядя Фёдор", :mccme 1}
+    {:name "Печкин", :mccme 2}
+  ],
   :contests
-  [{:name "Тема 1"
-    :problems [3443 3444]}
-   {:name "Тема 2"
-    :problems [3501 3502]}]}
- {:id "nez",
+  [
+    {:name "Тема 1", :problems [3443 3444]}
+    {:name "Тема 2", :problems [3501]}
+  ]
+}
+{
+  :id "nez",
   :name "Незнайка и его друзья",
-  :accept true
+  :accept true,
   :users
-  [{:name "Незнайка", :mccme 5}
-   {:name "Кнопочка", :mccme 6}
-   {:name "Синеглазка", :mccme 7}
-   {:name "Тюбик", :mccme 8}]
+  [
+    {:name "Незнайка", :mccme 3}
+    {:name "Тюбик", :mccme 4}
+  ],
   :contests
-  [{:name "Тренировка 1"
-    :problems [611 612 613]}
-   {:name "Тренировка 2"
-    :problems [617 618]}]}]
+  [
+    {:name "Тренировка 1",:problems [611 {:mccme 612, :name "Выражение"} 613]}
+  ]
+}
+]
