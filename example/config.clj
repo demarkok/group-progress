12b1843ae33635e0e5b1bf826cecb48b331aa51b
Oleg Pestov <oleg.pestov@gmail.com>
2016-07-11 09:17:47 +0300

    Add examples

---
+++ example/config.clj
@@ -0,0 +1,34 @@
+[{:id "prostokvashino",
+  :name "Простоквашино",
+  :users
+  [{:name "Дядя Фёдор", :mccme 1}
+   {:name "Матроскин", :mccme 2}
+   {:name "Шарик", :mccme 3}
+   {:name "Печкин", :mccme 4}],
+  :contests
+  [{:name "Тема 1",
+    :problems
+    [{:mccme 3443, :name "Степень двойки."}
+     {:mccme 3444, :name "Факториал."}]}
+   {:name "Тема 2",
+    :problems
+    [{:mccme 3501, :name "Максимум двух чисел"}
+     {:mccme 3502, :name "Какое число больше?"}]}]}
+ {:id "nez",
+  :name "Незнайка и его друзья",
+  :accept true,
+  :users
+  [{:name "Незнайка", :mccme 5}
+   {:name "Кнопочка", :mccme 6}
+   {:name "Синеглазка", :mccme 7}
+   {:name "Тюбик", :mccme 8}],
+  :contests
+  [{:name "Тренировка 1",
+    :problems
+    [{:mccme 611, :name "Простые числа"}
+     {:mccme 612, :name "Выражение"}
+     {:mccme 613, :name "Возрастающая подпоследовательность"}]}
+   {:name "Тренировка 2",
+    :problems
+    [{:mccme 617, :name "Простые числа(2)"}
+     {:mccme 618, :name "Перестановки"}]}]}]
---
+++ example/preconfig.clj
@@ -0,0 +1,25 @@
+[{:id "prostokvashino"
+  :name "Простоквашино"
+  :users
+  [{:name "Дядя Федор" :mccme 1}
+   {:name "Матроскин" :mccme 2}
+   {:name "Шарик" :mccme 3}
+   {:name "Печкин" :mccme 4}]
+  :contests
+  [{:name "Тема 1"
+    :problems [3443 3444]}
+   {:name "Тема 2"
+    :problems [3501 3502]}]}
+ {:id "nez",
+  :name "Незнайка и его друзья",
+  :accept true
+  :users
+  [{:name "Незнайка", :mccme 5}
+   {:name "Кнопочка", :mccme 6}
+   {:name "Синеглазка", :mccme 7}
+   {:name "Тюбик", :mccme 8}]
+  :contests
+  [{:name "Тренировка 1"
+    :problems [611 612 613]}
+   {:name "Тренировка 2"
+    :problems [617 618]}]}]
